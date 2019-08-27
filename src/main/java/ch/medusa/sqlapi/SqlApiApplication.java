220231643df54d74d901a0793a547f08c803d9c5
Severin Weigold <severin.weigold@edu.tbz.ch>
2019-08-27 16:35:03 +0200

    initial commit

---
+++ src/main/java/ch/medusa/sqlapi/SqlApiApplication.java
@@ -0,0 +1,13 @@
+package ch.medusa.sqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlApiApplication.class, args);
+	}
+
+}
